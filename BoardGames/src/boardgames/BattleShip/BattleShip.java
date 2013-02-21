908511f7af017a6d0cb2931fb128412eb1af4f76
Michael Stombaugh <mstombaugh@tamu.edu>
2013-02-21 13:45:53 -0600

    Added starting BattleShip code

--- BoardGames/src/boardgames/BattleShip/BattleShip.java
+++ BoardGames/src/boardgames/BattleShip/BattleShip.java
@@ -130,7 +130,7 @@ public class BattleShip extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    /*public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -155,12 +155,12 @@ public class BattleShip extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+       /* java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new BattleShip().setVisible(true);
             }
         });
-    }
+    }*/
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
